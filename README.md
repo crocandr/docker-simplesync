ff82a33267b553c920415a0a93e06ecf5cd9c9aa
crocandr <sandras@sharkfishweb.com>
2016-07-14 08:39:01 +0200

    mode switcher function added

--- README.md
+++ README.md
@@ -46,6 +46,22 @@ You have to define at start the generated (or and old ) key for sync:
 docker run -tid --name=simplesync --net=host -v /home/myuser/myfiles:/srv/sync -e KEY=AGDEDBKGWD7HOJJF6BUHSVU44Z5UBNZ46 my/simplesync /opt/start.sh
 ```
 
+## Mode
+
+You can change Bittorrent Sync mode with these params:
+
+  - not defined - readwrite mode/automatic
+  - `-e MODE=ro` - readonly mode
+  - `-e MODE=encrypt` - encrypted mode
+
+Example:
+
+```
+docker run -tid --name=simplesync --net=host -v /home/myuser/myfiles:/srv/sync -e KEY=AGDEDBKGWD7HOJJF6BUHSVU44Z5UBNZ46 -e MODE=ro my/simplesync /opt/start.sh
+```
+
+You have to define a normal RW key to the container generates a RO or a Encrpyted key.
+If you use a RO or an Encrypted key by default, You musn't define the mode switcher parameter. 
 
 
 Good luck!
--- files/start.sh
+++ files/start.sh
@@ -10,10 +10,45 @@ else
   key="$( /opt/btsync/btsync --generate-secret )"
 fi
 
+case $MODE in
+  "ro")
+    rokey="$( /opt/btsync/btsync --get-ro-secret $KEY )"
+    if [ ! -z $rokey ]
+    then
+      echo "Generated ReadOnly KEY: $rokey"
+      echo "---- MODE: ReadOnly ----"
+      key=$rokey
+    else
+      echo "ERR: ReadOnly key generate error"
+      exit 1
+    fi
+    ;;
+  "encrypt")
+    tmpkey="$( /opt/btsync/btsync --get-ro-secret $KEY )"
+    enckey=$( echo $tmpkey | cut -c-33 | sed -r "s/^(.{1})/F/" )
+    if [ ! -z $enckey ]
+    then
+      echo "Generated Encrypted KEY: $enckey"
+      echo "---- MODE: Encrypted ----"
+      key=$enckey
+    else
+      echo "ERR: Encrypted key generate error"
+      exit 1
+    fi
+    ;;
+  *)
+    echo "KEY: $key"
+    echo "---- MODE: Automatic (defined by your key) ----"
+    echo "Please copy this key to the other host if nessesary"
+    echo -e "\n\n\n"
+    ;;
+esac
+
+
 # change btsync conf
 sed -i s@--BTSYNCKEY--@$key@g /etc/btsync.conf
 sed -i s@--SRVNAME--@$HOSTNAME@g /etc/btsync.conf
-echo -e "\n\nPLEASE COPY THIS BTSYNC KEY to the other hosts: $key \n\n"
+#echo -e "\n\nPLEASE COPY THIS BTSYNC KEY to the other hosts: $key \n\n"
 echo $key >> /etc/btsync.key
 
 /opt/btsync/btsync --storage /opt/btsync/config --config /etc/btsync.conf --nodaemon
